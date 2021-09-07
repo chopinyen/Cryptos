fd39ac3ecc41acf4c2c39ae48a562c5f22e43074
Chopin Yen <chopin@drvigo.com>
2021-09-07 10:28:38 -0700

    Remove unused files and add coin name to the list

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -5,6 +5,7 @@ const Coin = ({coin, deleteCoin}) => {
     return (
         <Link to={`/coins/${coin.id}`} className="text-decoration-none my-1 coin">
             <li className="coinlist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center text-dark">
+                <span className="coin-display-name">{coin.name}</span>
                 <img className="coinlist-image" src={coin.image} alt="" />
                 <span className="text-decoration-none">{coin.current_price}</span>
 
